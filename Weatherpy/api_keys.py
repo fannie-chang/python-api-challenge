b50b638b4de954840f0f98582e7b0c672a1ba8b1
Fannie Chang <fanniechang@Fannies-MacBook-Pro.local>
2021-04-28 12:33:48 -0700

    update weatherpy and vacationpy v1

--- Weatherpy/api_keys.py
+++ Weatherpy/api_keys.py
@@ -1,5 +1,6 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "71c2ec27ddc74ebeac49d34fc97e759c"
+
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDn7-sWEaPVKHiUD_SxrTVrGJHJ4H8OQhE"
